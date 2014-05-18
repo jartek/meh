f5668f41bbad44d56e98f63e814dfb7f86419bec
jartek <jartek.91@gmail.com>
2014-05-18 13:54:56 +0530

    forced password

--- models/models.go
+++ models/models.go
@@ -11,19 +11,20 @@ import (
 )
 
 var DBConfig struct {
-	SslMode, Name, User string
+	SslMode, Name, User, Password string
 }
 
 func LoadDBConfig() {
 	DBConfig.Name = os.Getenv("DB_NAME")
 	DBConfig.SslMode = os.Getenv("DB_SSLMODE")
 	DBConfig.User = os.Getenv("DB_USER")
+	DBConfig.Password = os.Getenv("DB_PASSWORD")
 }
 
 func SetupDB() *sql.DB {
 	LoadDBConfig()
-	db, err := sql.Open("postgres", fmt.Sprintf("user=%s dbname=%s sslmode=%s",
-		DBConfig.User, DBConfig.Name, DBConfig.SslMode))
+	db, err := sql.Open("postgres", fmt.Sprintf("user=%s password=%s dbname=%s sslmode=%s",
+		DBConfig.User, DBConfig.Password, DBConfig.Name, DBConfig.SslMode))
 	if err != nil {
 		log.Fatalln(err)
 	}
